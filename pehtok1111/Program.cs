ababde08222e0f7f11eeddb2425258797bae6b28
pehtok <pehtok@HOME-PC>
2025-05-05 10:40:22 +0300

    Добавьте файлы проекта.

---
+++ pehtok1111/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pehtok1111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер телефона родителей");
+            string dfg;
+            dfg = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст");
+            string sao;
+            sao = Console.ReadLine();
+            int age;
+            age = Convert.ToInt32(sao);
+            if (age < 18)
+            {
+                Console.WriteLine("Малой, иди нахуй. Или я позвоню твоим родителям по этому номеру телефона: " + dfg);
+            }
+            if (age >= 18) 
+            {
+                Process.Start("http://vsexshop.ru");
+            }
+
+        }
+    }
+}
---
+++ pehtok1111/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("pehtok1111")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pehtok1111")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("14f2140d-f9b2-4220-aefa-505b586e208d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
